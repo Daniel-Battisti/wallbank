54494f0ff882174f56c55da627cf0cbf27df36e4
Victor H. F. Wachsmann <vhwachsmann@gmail.com>
2017-10-28 16:41:24 -0200

    Add files via upload

---
+++ src/com/univali/wallbank/controller/User.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.univali.wallbank.controller;
+
+/**
+ *
+ * @author wachsmann
+ */
+public class User implements UserBase{
+
+    @Override
+    public boolean login(String name, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void logout() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ src/com/univali/wallbank/factory/Client.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.univali.wallbank.factory;
+import com.univali.wallbank.controller.UserBase;
+
+/**
+ *
+ * @author wachsmann
+ */
+public interface Client extends UserBase{
+    
+}
